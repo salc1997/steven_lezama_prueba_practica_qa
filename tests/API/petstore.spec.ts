b7166293bcae59d023411d79d827508094fb8d74
salc1997 <lezamasteve@gmail.com>
2024-10-01 16:01:29 -0600

    Update petstore.spec.ts

--- tests/API/petstore.spec.ts
+++ tests/API/petstore.spec.ts
@@ -3,31 +3,15 @@ import { generateRandomUser } from '../page_object/testUtils'
 
 
 
-baseTest.describe('This are the positive tests for users', () => {
+const randomCreateData = generateRandomUser()
+const randomUpdateData = generateRandomUser()
 
-    let randomCreateData: any = {}
-    let randomUpdateData: any = {}
-
-    async function searchAndValidateUser(userPO: any, data: any) {
-        const response = await userPO.searchUser(randomCreateData.username);
-        const responseJson = await response.json();
-
-        expect(response.status()).toBe(200);
-        //expect(responseJson.id.toString()).toBe(data.id);
-        expect(responseJson.username).toBe(data.username);
-        expect(responseJson.email).toBe(data.email);
-        expect(responseJson.password).toBe(data.password);
-        expect(responseJson.phone).toBe(data.phone);
-        expect(responseJson.userStatus.toString()).toBe(data.userStatus);
-    }
+baseTest.describe('This are the test to create and validate an user', () => {
 
     baseTest('This is a test to create a user', async ({ userPO }) => {
 
-        randomCreateData = generateRandomUser()
         const response = await userPO.createUser(randomCreateData);
         const responseJson = await response.json()
-
-        console.log('test data create: ', randomUpdateData)
 
         expect(responseJson.code).toBe(200)
         expect(responseJson.type).toBe('unknown')
@@ -37,18 +21,19 @@ baseTest.describe('This are the positive tests for users', () => {
 
     baseTest('This is a test to validate the created user', async ({ userPO }) => {
 
-        await searchAndValidateUser(userPO, randomCreateData)
+        await userPO.searchAndValidateUser(randomCreateData)
 
     })
 
-    baseTest('This is a test to update the user', async ({ userPO }) => {
+})
 
-        randomUpdateData = generateRandomUser()
+baseTest.describe('This are the test to update and validate an user', () => {
+
+
+    baseTest('This is a test to update the user', async ({ userPO }) => {
 
         randomUpdateData.id = randomCreateData.id
         randomUpdateData.username = randomCreateData.username
-
-        console.log('test data update: ', randomUpdateData)
 
         const response = await userPO.updateUser(randomCreateData.username, randomUpdateData)
         const responseJson = await response.json()
@@ -58,9 +43,10 @@ baseTest.describe('This are the positive tests for users', () => {
 
     })
 
+
     baseTest('This is a test to validate the updated user', async ({ userPO }) => {
 
-        await searchAndValidateUser(userPO, randomUpdateData)
+        await userPO.searchAndValidateUser(randomUpdateData)
 
     })
 
