a241fcd4adca5c28be1b608de864893c9f4c4a04
salc1997 <lezamasteve@gmail.com>
2024-10-01 14:39:15 -0600

    Update UserPO.ts

--- tests/API/page_object/UserPO.ts
+++ tests/API/page_object/UserPO.ts
@@ -1,39 +1,34 @@
-import { APIRequestContext, expect } from "@playwright/test";
+import { APIRequestContext } from "@playwright/test";
 import * as testData from "../../fixtures/testData.json"
 
 export class UserPO {
 
 
-    private request: APIRequestContext
+    private readonly request: APIRequestContext
     baseURL = testData.apiURL
     constructor(request: APIRequestContext) {
         this.request = request
     }
 
-    /*async login(userData) {
-        const response = await this.request.post(`https://petstore.swagger.io/#/user/`, {
-            data: {
-                "username": "eve.holt@reqres.in",
-                "password": "cityslicka"
-            }
-        })
-
-        //verify that the response is 200
-        expect(response.status()).toBe(201)
-
-        const responseBody = await response.json()
-        console.log(responseBody)
-
-        return response
-    }*/
-
     async createUser(userData) {
 
-        const response = await this.request.post(`${this.baseURL}/createUser`, {
+        const response = await this.request.post(`${this.baseURL}/user`, {
             data: userData
         })
-        console.log(response.ok())
-        expect(response.ok())
+        return response
+    }
+
+    async updateUser(username, randomData) {
+
+        const response = await this.request.put(`${this.baseURL}/user/${username}`, {
+            data: randomData
+        })
+        return response
+    }
+
+    async searchUser(username) {
+
+        const response = await this.request.get(`${this.baseURL}/user/${username}`)
 
         return response
     }
