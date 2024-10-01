a39ae517940fb978f6e8562545c0604e69ce67dc
salc1997 <lezamasteve@gmail.com>
2024-10-01 14:47:55 -0600

    Update testUtils.ts

--- tests/page_object/testUtils.ts
+++ tests/page_object/testUtils.ts
@@ -30,3 +30,38 @@ export const getRandomInt = (length: number): number => {
     let number = Math.floor(Math.random() * (length - 1) + 1)
     return number;
 }
+
+export const generateRandomUser = () => {
+
+    const firstNames = ['John', 'Jane', 'Michael', 'Emily', 'David', 'Emma', 'Chris', 'Olivia', 'James', 'Sophia'];
+    const lastNames = ['Smith', 'Johnson', 'Brown', 'Williams', 'Jones', 'Garcia', 'Miller', 'Davis', 'Rodriguez', 'Martinez'];
+
+    const getRandomNumber = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+    const getRandomElement = (array: string[]) => array[Math.floor(Math.random() * array.length)];
+    const getRandomEmail = (name: string, lastname: string) => {
+        const domains = ['example.com', 'mail.com', 'test.com'];
+        return `${name.toLowerCase()}.${lastname.toLowerCase()}@${domains[Math.floor(Math.random() * domains.length)]}`;
+    };
+
+    const id = getRandomNumber(1000, 9999).toString();
+    const name = getRandomElement(firstNames);
+    const lastname = getRandomElement(lastNames);
+    const username = `${name}.${lastname}`
+    const email = getRandomEmail(name, lastname);
+    const password = Math.random().toString(36).slice(-10);
+    const phone = getRandomNumber(1000000000, 9999999999).toString();
+    const userStatus = getRandomNumber(0, 10).toString();
+
+    const randomData = {
+        id,
+        username,
+        name,
+        lastname,
+        email,
+        password,
+        phone,
+        userStatus
+    };
+
+    return randomData
+}
